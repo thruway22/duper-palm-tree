76594f1b725787d39a3e6812c76ec503fedca60e
thruway22 <121792615+thruway22@users.noreply.github.com>
2024-01-04 22:00:22 +0300

    u

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 import auth 
 
 db = auth.Connect()
@@ -17,6 +18,7 @@ data = {
     "stringExample": "Hello, World!",
     "booleanExample": True,
     "numberExample": 3.14159265,
+    "dateExample": datetime.datetime.now(tz=datetime.timezone.utc),
     "arrayExample": [5, True, "hello"],
     "nullExample": None,
     "objectExample": {"a": 5, "b": True},

a0c345b529636a2d0b832451b8bb9397c41142ab
thruway22 <121792615+thruway22@users.noreply.github.com>
2024-01-04 22:00:01 +0300

    u

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ data = {
     "stringExample": "Hello, World!",
     "booleanExample": True,
     "numberExample": 3.14159265,
-    "dateExample": datetime.datetime.now(tz=datetime.timezone.utc),
     "arrayExample": [5, True, "hello"],
     "nullExample": None,
     "objectExample": {"a": 5, "b": True},
